32675f5baea8b7aa73121977b8e2c5e32c6b68eb
LAPTOP-VD8QGHIV\dyj <1278247649@qq.com>
2025-10-31 23:59:51 +0800

    编译

---
+++ IAP/MDK-ARM/Debug/croutine.d
@@ -0,0 +1,14 @@
+.\debug\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/croutine.c
+.\debug\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+.\debug\croutine.o: D:\MDK\CORE\ARM\ARMCC\Bin\..\include\stddef.h
+.\debug\croutine.o: D:\MDK\CORE\ARM\ARMCC\Bin\..\include\stdint.h
+.\debug\croutine.o: ../Core/Inc/FreeRTOSConfig.h
+.\debug\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+.\debug\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+.\debug\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+.\debug\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+.\debug\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+.\debug\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+.\debug\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+.\debug\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/croutine.h
+.\debug\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
---
+++ IAP/MDK-ARM/Debug/queue.d
@@ -0,0 +1,15 @@
+.\debug\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/queue.c
+.\debug\queue.o: D:\MDK\CORE\ARM\ARMCC\Bin\..\include\stdlib.h
+.\debug\queue.o: D:\MDK\CORE\ARM\ARMCC\Bin\..\include\string.h
+.\debug\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+.\debug\queue.o: D:\MDK\CORE\ARM\ARMCC\Bin\..\include\stddef.h
+.\debug\queue.o: D:\MDK\CORE\ARM\ARMCC\Bin\..\include\stdint.h
+.\debug\queue.o: ../Core/Inc/FreeRTOSConfig.h
+.\debug\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+.\debug\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+.\debug\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+.\debug\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+.\debug\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+.\debug\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+.\debug\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+.\debug\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/queue.h
